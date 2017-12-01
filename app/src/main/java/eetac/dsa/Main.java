41a47fc19e4e17c1ac4552a7e04bb3e3b29b58a4
Albert <albertalvarezmiron@gmail.com>
2017-12-01 10:59:25 +0100

    Inicio

--- app/src/main/java/eetac/dsa/Main.java
+++ app/src/main/java/eetac/dsa/Main.java
@@ -2,13 +2,74 @@ package eetac.dsa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity
 {
+    EditText user;
+    EditText pass;
+    CheckBox check;
+    Button btnIniciar;
+    Button btnRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (EditText) findViewById(R.id.userBox);
+        pass = (EditText) findViewById(R.id.passBox);
+        check = (CheckBox) findViewById(R.id.checkBox);
+        btnIniciar = (Button) findViewById(R.id.btnIniciarSesion);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrarse);
+
+        check.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (check.isChecked()) {  pass.setTransformationMethod(null);  }
+
+                else {  pass.setTransformationMethod(new PasswordTransformationMethod());  }
+            }
+        }
+        );
+
+        btnIniciar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (TextUtils.isEmpty(user.getText().toString()) || TextUtils.isEmpty(pass.getText().toString()))
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Campos incompletos", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Iniciar Sesión", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        );
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast toast = Toast.makeText(getApplicationContext(), "Registrar", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        );
     }
 }
